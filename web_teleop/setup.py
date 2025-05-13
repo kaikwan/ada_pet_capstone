e4e1b742337ad0550f5346dfd7d3854fa158478d
Henri Fung <kaikwanfung@gmail.com>
2025-05-12 20:58:56 -0700

    Cleanup align_to_aruco.py and formatting

--- web_teleop/setup.py
+++ web_teleop/setup.py
@@ -11,7 +11,7 @@ setup(
         ("share/ament_index/resource_index/packages", ["resource/" + package_name]),
         ("share/" + package_name, ["package.xml"]),
         ("share/" + package_name + "/launch", glob("launch/*.launch.py")),
-        ('share/' + package_name + '/config', glob('config/*')),
+        ("share/" + package_name + "/config", glob("config/*")),
     ],
     install_requires=["setuptools"],
     zip_safe=True,
@@ -21,8 +21,6 @@ setup(
     license="TODO: License declaration",
     # tests_require=['pytest'],
     entry_points={
-        "console_scripts": [
-            "align_to_aruco = web_teleop.align_to_aruco:main"
-        ],
+        "console_scripts": ["align_to_aruco = web_teleop.align_to_aruco:main"],
     },
 )
